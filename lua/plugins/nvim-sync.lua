6dcee2eec5f0aaddbcfcfc8390a28fd3b1d0997c
Pedro Resende <pedroresende@mail.resende.biz>
2025-07-20 16:16:45 +0100

    Auto-sync nvim config - 2025-07-20 16:16:45

--- lua/plugins/nvim-sync.lua
+++ lua/plugins/nvim-sync.lua
@@ -6,6 +6,6 @@ return {
     -- Optional configuration
     auto_sync = false, -- Set to true for automatic sync on save
     sync_on_startup = true, -- Sync on Neovim startup
-    branch = "test", -- Git branch to use
+    branch = "main", -- Git branch to use
   },
 }
