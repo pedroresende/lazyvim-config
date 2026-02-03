c390a3a73ad1c46684820184e0cc3059a49ed4ec
Pedro Resende <pedroresende@users.noreply.github.com>
2026-02-03 08:31:34 +0000

    feat: cleanup & added neotest

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,4 +7,4 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")i
 --
-vim.cmd("colorscheme nordfox")
+-- vim.cmd("colorscheme nordfox")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  { colorscheme = "nordfox", import = "colors" },
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
